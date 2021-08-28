6971dfaf8ee822d96aa98997d96aa1c65f36bacc
Viktor <47147163+ViktorBrilko@users.noreply.github.com>
2021-08-28 21:20:34 +0300

    3 пункта второго дз

--- sandbox/src/main/java/course/sandbox/Hello.java
+++ sandbox/src/main/java/course/sandbox/Hello.java
@@ -3,7 +3,18 @@ package course.sandbox;
 public class Hello {
 
 	public static void main(String[] args) {
+		Point P1 = new Point(1,5);
+		Point P2 = new Point(7,10);
 
-		System.out.println("course.sandbox.Hello, world!");
+		System.out.println("Расстояние между точками - " + distance(P1, P2));
+
 	}
+
+	public static double distance(Point P1, Point P2){
+		return Math.sqrt((Math.pow((P1.p2-P1.p1),2)+Math.pow((P2.p2-P2.p1),2)));
+	}
+
 }
+
+
+
