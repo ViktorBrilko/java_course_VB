de85b5cf3af0055381cd54bdccf20b164ee35402
Viktor <47147163+ViktorBrilko@users.noreply.github.com>
2021-08-29 21:09:56 +0300

    4 пункт второго дз

--- sandbox/src/main/java/course/sandbox/Hello.java
+++ sandbox/src/main/java/course/sandbox/Hello.java
@@ -3,16 +3,17 @@ package course.sandbox;
 public class Hello {
 
 	public static void main(String[] args) {
-		Point P1 = new Point(1,5);
-		Point P2 = new Point(7,10);
+		Point p1 = new Point(1,5);
+		Point p2 = new Point(7,10);
 
-		System.out.println("Расстояние между точками - " + distance(P1, P2));
+		System.out.println("Расстояние между точками - " + p1.distance(p2));
+
 
 	}
 
-	public static double distance(Point P1, Point P2){
-		return Math.sqrt((Math.pow((P1.p2-P1.p1),2)+Math.pow((P2.p2-P2.p1),2)));
-	}
+//	public static double distance(Point P1, Point P2){
+//		return Math.sqrt((Math.pow((P1.p2-P1.p1),2)+Math.pow((P2.p2-P2.p1),2)));
+//	}
 
 }
 
