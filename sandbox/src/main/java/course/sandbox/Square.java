e43c272254555fd6f8ae837a92289ec883188476
Viktor <47147163+ViktorBrilko@users.noreply.github.com>
2021-08-30 10:25:36 +0300

    Создание класса Square и тестов к нему

---
+++ sandbox/src/main/java/course/sandbox/Square.java
@@ -0,0 +1,13 @@
+package course.sandbox;
+
+public class Square {
+    public double l;
+
+    public Square(double l ) {
+        this.l = l;
+    }
+
+    public double area(){
+        return this.l * this.l;
+    }
+}
