0900e34d1242c311a5806d06100bd40421f30616
Viktor <47147163+ViktorBrilko@users.noreply.github.com>
2021-09-13 00:14:33 +0300

    Перенос logout и создание ContactHelper
    Перенос logout в отдельный метод в SessionHelper

---
+++ addressbook-web-tests/src/test/java/course/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,41 @@
+package course.addressbook.appmanager;
+
+import course.addressbook.models.ContactData;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ContactHelper extends HelperBase{
+
+    public ContactHelper(WebDriver wd) {
+        super(wd);
+    }
+
+    public void fillContactForm(ContactData contactData) {
+        type(By.name("firstname"),contactData.getName());
+        type(By.name("middlename"),contactData.getMiddleName());
+        type(By.name("lastname"),contactData.getLastName());
+        type(By.name("nickname"),contactData.getNickname());
+        type(By.name("title"),contactData.getTitle());
+        type(By.name("company"),contactData.getCompany());
+        type(By.name("address"),contactData.getAddress());
+        type(By.name("home"),contactData.getHome());
+        type(By.name("mobile"),contactData.getMobile());
+        type(By.name("work"),contactData.getWork());
+        type(By.name("fax"),contactData.getFax());
+        type(By.name("email"),contactData.getEmail1());
+        type(By.name("email2"),contactData.getEmail2());
+        type(By.name("email3"),contactData.getEmail3());
+        type(By.name("homepage"),contactData.getHomepage());
+        type(By.name("address2"),contactData.getAddress2());
+        type(By.name("phone2"),contactData.getHome2());
+        type(By.name("notes"),contactData.getNotes());
+    }
+
+    public void submitContactCreation() {
+        click(By.xpath("//div[@id='content']/form/input[21]"));
+    }
+
+    public void initContactCreation() {
+        click(By.linkText("add new"));
+    }
+}
--- addressbook-web-tests/src/test/java/course/addressbook/tests/NewContactCreationTest.java
+++ addressbook-web-tests/src/test/java/course/addressbook/tests/NewContactCreationTest.java
@@ -7,9 +7,9 @@ public class NewContactCreationTest extends TestBase{
 
   @Test
   public void testNewContactCreation() throws Exception {
-    app.initContactCreation();
-    app.fillContactForm(new ContactData("name", "middle name", "last name", "nickname", "title", "company", "address", "home", "mobile", "work", "fax", "email1", "email2", "email3", "homepage", "address2", "home2", "notes"));
-    app.submitContactCreation();
+    app.getContactHelper().initContactCreation();
+    app.getContactHelper().fillContactForm(new ContactData("name", "middle name", "last name", "nickname", "title", "company", "address", "home", "mobile", "work", "fax", "email1", "email2", "email3", "homepage", "address2", "home2", "notes"));
+    app.getContactHelper().submitContactCreation();
     app.getNavigationHelper().goToHomePage();
   }
 
