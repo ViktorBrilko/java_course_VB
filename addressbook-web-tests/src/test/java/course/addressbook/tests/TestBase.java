a93f3d00664db15389e6f15d0290370daa63beea
Viktor <47147163+ViktorBrilko@users.noreply.github.com>
2021-09-19 17:13:17 +0300

    Добавление возможности запуска тестов в разных браузерах

--- addressbook-web-tests/src/test/java/course/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/course/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package course.addressbook.tests;
 
 import course.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.EDGE);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
