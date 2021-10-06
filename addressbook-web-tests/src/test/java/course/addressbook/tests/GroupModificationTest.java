b5e64c75a29b9e0a5576a29057a8969e48d762bf
Viktor <47147163+ViktorBrilko@users.noreply.github.com>
2021-10-06 23:11:49 +0300

    Добавил сравнение списков после модификации группы

--- addressbook-web-tests/src/test/java/course/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/course/addressbook/tests/GroupModificationTest.java
@@ -4,6 +4,7 @@ import course.addressbook.models.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTest extends TestBase{
@@ -17,10 +18,15 @@ public class GroupModificationTest extends TestBase{
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("TEST", "TEST", "TEST"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(),"TEST", "TEST", "TEST");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getNavigationHelper().goToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
