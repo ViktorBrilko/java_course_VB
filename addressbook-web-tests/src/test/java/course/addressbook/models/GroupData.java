d72d198913d5612258ef211cce90ad8c6c4b391b
Viktor <47147163+ViktorBrilko@users.noreply.github.com>
2021-10-07 19:18:25 +0300

    Добавил сравнение списка групп при их создании

--- addressbook-web-tests/src/test/java/course/addressbook/models/GroupData.java
+++ addressbook-web-tests/src/test/java/course/addressbook/models/GroupData.java
@@ -3,17 +3,33 @@ package course.addressbook.models;
 import java.util.Objects;
 
 public class GroupData {
-    private final String id;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public GroupData(int id, String name, String header, String footer) {
+        this.id = id;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(id, groupData.id) && Objects.equals(name, groupData.name);
+        return id == groupData.id && Objects.equals(name, groupData.name);
     }
 
     @Override
@@ -29,18 +45,8 @@ public class GroupData {
                 '}';
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public GroupData(String id, String name, String header, String footer) {
-        this.id = id;
-        this.name = name;
-        this.header = header;
-        this.footer = footer;
-    }
     public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
